bdd9c94c959b01000dcfc7c0c147c71d1150df15
Sam Slotsky <sslotsky@users.noreply.github.com>
2017-08-24 12:44:37 -0500

    Fixes for max page# and currentQuery bugs (#12)

--- src/pageData.js
+++ src/pageData.js
@@ -5,7 +5,7 @@ export default function pageData(currentPage, totalPages, maxPages = 7) {
   const hasLeftSeparator = (totalPages > maxPages) && (currentPage > 2)
   const minPage = hasLeftSeparator ? Math.min(currentPage, totalPages - (maxPages - 1)) : 2
   const rangeSize = maxPages - [hasRightSeparator, hasLeftSeparator].filter(h => h).length
-  const maxPage = Math.max(minPage + 1, Math.min(totalPages - 1, minPage + (rangeSize - 1)))
+  const maxPage = Math.max(minPage, Math.min(totalPages - 1, minPage + (rangeSize - 1)))
 
   const firstPage = {
     page: 1,
