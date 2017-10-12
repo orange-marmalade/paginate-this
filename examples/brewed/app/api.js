9dd50c9db227c609437cdb54a32da0a80b40f7a5
Sam Slotsky <saxosamo@gmail.com>
2017-10-12 15:18:21 -0500

    React 16

--- examples/brewed/app/api.js
+++ examples/brewed/app/api.js
@@ -5,7 +5,6 @@ const API_BASE = 'https://brewed-dev.herokuapp.com/v1'
 function api() {
   const adapter = axios.create({
     baseURL: API_BASE,
-    timeout: 10000,
     withCredentials: true,
     headers: {
       'X-Api-Key': 'b780aac581de488cf77a629517ac999b',
